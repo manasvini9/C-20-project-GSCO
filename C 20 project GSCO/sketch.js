8cd0d34bb4c4c7e4863c6011309f5d98285eddba
manasvini9 <74662223+manasvini9@users.noreply.github.com>
2020-12-12 17:07:06 +0530

    Add files via upload

---
+++ C 20 project GSCO/sketch.js
@@ -0,0 +1,54 @@
+var car, wall; 
+var safeimg,lethalimg,averageimg,mainimg;
+var speed, weight; 
+
+function preload(){
+
+ mainimg=loadImage("main.jpg");
+ safeimg=loadImage("safe.jpg");
+ averageimg=loadImage("average.jpg");
+ lethalimg=loadImage("lethal.jpg");
+
+}
+
+function setup() {
+
+  createCanvas(1400,400);
+
+  speed=random(55,90);
+  weight=random(400,1500);
+
+  car=createSprite(50, 200, 50,50);
+  car.shapeColor="white";
+  car.addImage(mainimg);
+  car.scale=0.5;
+  car.addAnimation("safe",safeimg);
+  car.addAnimation("average",averageimg);
+  car.addAnimation("lethal",lethalimg);
+  car.velocityX=speed;
+
+  wall=createSprite(1500, 200, 60, height/2);
+  wall.shapeColor="grey";
+
+}
+
+function draw() {
+  background("black");
+
+  if (wall.x-car.x < (car.width+wall.width)/2)
+  {
+    car.velocityX = 0;
+    var deformation = 0.5 * weight * speed * speed/22509;
+    if (deformation>180)
+    {
+      car.shapeColor=color(255,0,0);
+    }
+    if (deformation<100)
+    {
+      car.shapeColor=color(0,255,0);
+    }
+
+  }
+  }
+  drawSprites();
+
